7c2ca8b34b92894b2a9f028bbae8e27758ff41cc
ShivangiModi <smodi0123@gmail.com>
2025-05-05 19:04:16 -0700

    Update Retail_Checkout.py

--- Retail_Checkout.py
+++ Retail_Checkout.py
@@ -165,6 +165,6 @@ def download_bill():
 
 if __name__ == '__main__':
     print("Server is starting...")
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
 
